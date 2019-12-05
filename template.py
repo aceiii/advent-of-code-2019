9c2630fe66cc6e954e7ad861ac8e9db03a984a28
Borin Ouch <borin.ouch@gmail.com>
2019-12-05 00:42:25 -0800

    Add template.py

---
+++ template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def part1(file):
+    # TOOD: First part of day
+    pass
+
+
+def part2(file):
+    # TOOD: Second part of day
+    pass
+
+
+def main(part, file):
+    if file.isatty():
+        print("Awaiting input from stdin...")
+
+    if part == 1:
+        part1(file)
+    elif part == 2:
+        part2(file)
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2 or len(sys.argv) > 3 or (sys.argv[1] != "1" and sys.argv[1] != "2")):
+        print(f"usage: {sys.argv[0]} part [filename]")
+        print(f"")
+        exit(1)
+
+    input_file = sys.stdin
+
+    if (len(sys.argv) == 3):
+        try:
+            filename = sys.argv[2]
+            input_file = open(filename)
+        except FileNotFoundError as e:
+            print(e)
+            exit(1)
+
+    with input_file:
+        main(int(sys.argv[1], 10), input_file)
