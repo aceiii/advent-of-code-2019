6d249173fe032a0d94c53a6b915bfd2be0a41f50
Borin Ouch <borin.ouch@gmail.com>
2019-12-05 20:51:23 -0800

    Implement day 4 part 2

--- day4.py
+++ day4.py
@@ -3,7 +3,7 @@
 import sys
 
 
-def match_rules(val):
+def match_rules1(val):
     digits = list(map(int, str(val)))
     has_adj_duplicate = False
 
@@ -18,15 +18,34 @@ def match_rules(val):
     return has_adj_duplicate
 
 
+def match_rules2(val):
+    digits = list(map(int, str(val)))
+    digit_count = {}
+    digit_count[digits[0]] = 1
+
+    for index in range(1, len(digits)):
+        current = digits[index]
+        previous = digits[index - 1]
+        if current < previous:
+            return False
+        elif previous == current:
+            digit_count[current] += 1
+        else:
+            digit_count[current] = 1
+
+    return 2 in digit_count.values()
+
+
 def part1(file):
     start, end = list(map(lambda x: int(x, 10), file.read().split('-')))
-    count = sum(1 for i in range(start, end+1) if match_rules(i))
+    count = sum(1 for i in range(start, end+1) if match_rules1(i))
     print(f"Answer: {count}")
 
 
 def part2(file):
-    # TOOD: Second part of day
-    pass
+    start, end = list(map(lambda x: int(x, 10), file.read().split('-')))
+    count = sum(1 for i in range(start, end+1) if match_rules2(i))
+    print(f"Answer: {count}")
 
 
 def main(part, file):
