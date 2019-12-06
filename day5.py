84eccf7a89b0bb49e26f8287547f50eb7ed427c8
Borin Ouch <borin.ouch@gmail.com>
2019-12-05 23:12:43 -0800

    Implement day 5 part 2

--- day5.py
+++ day5.py
@@ -2,7 +2,7 @@
 
 import sys
 from enum import Enum
-from operator import add, mul
+from operator import add, mul, lt, eq, truth, not_
 
 
 class OpCode(Enum):
@@ -10,6 +10,10 @@ class OpCode(Enum):
     MULTIPLY = 2
     INPUT = 3
     OUTPUT = 4
+    JMP_IF_TRUE = 5
+    JMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
     HALT = 99
 
 
@@ -37,7 +41,7 @@ class Computer(object):
 
     def step(self, size):
         self._index += size
-        return size
+        return self.op() != OpCode.HALT
 
     def data(self):
         return self._data[:]
@@ -91,10 +95,26 @@ class Computer(object):
             return 2
 
         if op_code is OpCode.OUTPUT:
-            index = self.param(0, True)
-            value = self.get(index)
-            print(f"{index:06d}: {value}")
+            value = self.param(0)
+            print(f"OUTPUT: {value}")
             return 2
+
+        if op_code is OpCode.JMP_IF_TRUE or op_code is OpCode.JMP_IF_FALSE:
+            test = self.param(0)
+            target = self.param(1)
+            op = truth if op_code is OpCode.JMP_IF_TRUE else not_
+            jump = 3
+            if op(test):
+                jump = target - self.index()
+            return jump
+
+        if op_code is OpCode.LESS_THAN or op_code is OpCode.EQUALS:
+            num1 = self.param(0)
+            num2 = self.param(1)
+            index = self.param(2, True)
+            op = lt if op_code is OpCode.LESS_THAN else eq
+            self.set(index, 1 if op(num1, num2) else 0)
+            return 4
 
     def run(self):
         while self.step(self.tick()):
@@ -110,8 +130,11 @@ def part1(file):
 
 
 def part2(file):
-    # TOOD: Second part of day
-    pass
+    op_codes = list(map(lambda x: int(x, 10), file.read().split(',')))
+    computer = Computer(op_codes)
+    print("START ====================")
+    computer.run()
+    print("END ======================")
 
 
 def main(part, file):
