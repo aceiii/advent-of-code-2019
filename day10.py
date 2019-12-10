1931224704e95ef28a64cbfc417a7d7aed6aca21
Borin Ouch <borin.ouch@gmail.com>
2019-12-10 02:24:19 -0800

    Implement day 10 part 2

--- day10.py
+++ day10.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-from math import atan2
+from math import pi, atan2
+from operator import itemgetter
+from collections import deque
 
 
 def detect_asteroids(asteroids, source):
@@ -13,6 +15,31 @@ def detect_asteroids(asteroids, source):
         x, y = asteroid
         angles.add(atan2(y - sy, x - sx))
     return len(angles)
+
+
+def asteroid_sort(asteroids, station):
+    sx, sy = station
+    angles = {}
+    for asteroid in asteroids:
+        if asteroid is station:
+            continue
+        x, y = asteroid
+        dx, dy = x - sx, y - sy
+        angle = atan2(dy, dx) + (pi / 2.0)
+        if angle < 0:
+            angle = (2 * pi) + angle
+        dist = dx * dx + dy * dy
+        if angle not in angles:
+            angles[angle] = []
+        angles[angle].append((dist, (x, y)))
+
+    sorted_asteroids = []
+    for key in sorted(angles.keys()):
+        sorted_tuples = sorted(angles[key], key=itemgetter(0))
+        queue = deque(map(itemgetter(1), sorted_tuples))
+        sorted_asteroids.append(queue)
+
+    return sorted_asteroids
 
 
 def part1(file):
@@ -33,8 +60,31 @@ def part1(file):
 
 
 def part2(file):
-    # TOOD: Second part of day
-    pass
+    lines = map(lambda x: x.strip(), file.readlines())
+
+    asteroids = []
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c != '#':
+                continue
+            asteroids.append((x, y))
+
+    detections = [(detect_asteroids(asteroids, asteroid), asteroid)
+                  for asteroid in asteroids]
+
+    _, station = sorted(detections, key=itemgetter(0), reverse=True)[0]
+
+    sorted_asteroids = asteroid_sort(asteroids, station)
+    popped = []
+    index = 0
+    while True:
+        if len(sorted_asteroids[index]):
+            popped.append(sorted_asteroids[index].popleft())
+        index = (index + 1) % len(sorted_asteroids)
+        if len(popped) == 200:
+            break
+    x, y = popped[-1]
+    print(f"Answer: {(x * 100) + y}")
 
 
 def main(part, file):
